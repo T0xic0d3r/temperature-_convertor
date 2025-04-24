d45aa539a7d4cac50654dde866fbe2b663e60d43
Ezra-Bett <ebett999@gmail.com>
2025-04-24 22:24:24 +0300

    Update functions.cs

--- functions.c
+++ functions.c
@@ -37,3 +37,10 @@ int main() {
     printf("6. Kelvin to Fahrenheit\n");
     printf("Enter your choice: ");
     scanf("%d", &choice)
+printf("Enter temperature: ");
+    scanf("%f", &temp);
+
+    switch (choice) {
+        case 1: celsiusToFahrenheit(temp); break;
+        case 2: celsiusToKelvin(temp); break;
+        case 3: fahrenheitToCelsius(temp); break;
